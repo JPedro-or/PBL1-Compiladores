527c64730b9fedb98b214ca324e874be39359545
João Pedro O. Rocha <jopedro.or@gmail.com>
2021-08-30 09:47:12 -0300

    entrada/saída nos arquivos input e output

--- runLexer.py
+++ runLexer.py
@@ -1,7 +1,7 @@
 import lexHolder_sm
 import os.path
 
-with open("input-lex.txt") as file:
+with open("input\entrada.txt") as file:
             #data = file.readlines()
             text = file.read() #in case it can read token by token with this methods
 
@@ -9,30 +9,30 @@ result, errors = lexHolder_sm.run(text)
 
 re_size = len(result)
 er_size = len(errors)
-open('out-lex.txt', 'w').close()#limpa o arquivo de saída para o novo output
+open('output\saida.txt', 'w').close()#limpa o arquivo de saída para o novo output
 count = -1
 for i in range(re_size):
     count = count + 1
     if count == 2:#quebra de linha pra cada token
         count = 0
-        with open('out-lex.txt', 'a') as file:
+        with open('output\saida.txt', 'a') as file:
             file.write('\n')
     if count != 2:
-        with open('out-lex.txt', 'a') as file:
+        with open('output\saida.txt', 'a') as file:
             file.write(str(result[i]))
             file.write(' ')
 
-with open('out-lex.txt', 'a') as file:
+with open('output\saida.txt', 'a') as file:
     file.write('\n')
 
 for i in range(er_size):
     count = count + 1
     if count == 2:#quebra de linha pra cada token
         count = 0
-        with open('out-lex.txt', 'a') as file:
+        with open('output\saida.txt', 'a') as file:
             file.write('\n')
     if count != 2:
-        with open('out-lex.txt', 'a') as file:
+        with open('output\saida.txt', 'a') as file:
             file.write(str(errors[i]))
             file.write(' ')
 print(result)
